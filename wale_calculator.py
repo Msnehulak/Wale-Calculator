56e15a9fbcc16f9e845cb0860a9b87c956ab3859
Msnehulak <mxmhrmn@gmail.com>
2026-04-22 12:50:18 +0200

    litle rewrite and add API 5* characters

--- wale_calculator.py
+++ wale_calculator.py
@@ -1,8 +1,10 @@
 from datetime import date
+import requests
 
 class whalecalculator:
     def __init__(self):
-        self.total = 0
+        self.standart5char = ["Jean", "Diluc", "Qiqi", "Mona", "Keqing", "Tighnari", "Dehya", "Mizuki"]
+        self.limited5char = self.get_limited_character_count()
         self.prices = {
             "welkin": 4.99,
             "battle_pass": 9.99,
@@ -15,41 +17,69 @@ class whalecalculator:
                 [6480, 99.99]
             ]
         }
-
+        self.primo = {
+            "bplvup": 150,
+            "refil rezin": 50,
+            "pull": 160
+        }
+        self.total_spend = 0
             # Date
-        self.releasedate = date(2020, 9, 28)
-        self.todaydate = date.today()
-        self.daysfromreleas = self.todaydate - self.releasedate
-        self.daysfromreleas = self.daysfromreleas.days
-        self.versincrelease = self.daysfromreleas // (7 * 6) 
+        self.release_date = date(2020, 9, 28)
+        self.today_date = date.today()
+        self.days_from_releas = self.today_date - self.release_date
+        self.days_from_releas = self.days_from_releas.days
+        self.versin_released = self.days_from_releas // (7 * 6) 
 
-            # Welkin Moon
-        self.welkinowned = self.daysfromreleas // 30
-        self.spendwelkin = self.welkinowned * self.prices["welkin"]
-        self.total += self.spendwelkin
-
-            # Batle pass
-        self.batlepassownd = self.versincrelease
-        self.spendbp = self.batlepassownd * self.prices["battle_pass"]
+        self.Welkin_Moon()
+        self.Batle_pass()
+        self.Batle_pass_levl_up()
+        self.Daly_resin_refill()
         
-        self.promobplv = 150
-        self.bpleve = self.primo_to_usd(self.promobplv)
-        self.maxlvbp = self.bpleve * 50
-        self.spendbplv = self.maxlvbp * self.batlepassownd 
-
-        self.total += self.spendbp + self.spendbplv
-
-            # characters
-        self.total5char = 65 
-        self.standart5char = 8
-        self.limited5char = self.total5char - self.standart5char 
+    def Welkin_Moon(self):
+        self.welkin_moon_owned = self.days_from_releas // 30 
+        self.welkin_moon_spend = self.welkin_moon_owned * self.prices["welkin"]
         
-        self.pulls1copy = 90 * 2
-        self.pullsC6 = self.pulls1copy * 7
-        self.priceC6 = self.primo_to_usd(self.pullsC6)
-        self.spendallchar = self.priceC6 * self.limited5char
+        self.total_spend += self.welkin_moon_spend
 
-        self.total += self.spendallchar
+    def Batle_pass(self):
+        self.BP_owned = self.versin_released
+        self.BP_spend = self.BP_owned * self.prices["battle_pass"]
+        
+        self.total_spend += self.BP_spend
+
+    def Batle_pass_levl_up(self):
+        self.BP_LV_UP_count = self.BP_owned * 50
+        self.BP_LV_UP_spend_primo = self.BP_LV_UP_count * self.primo["bplvup"]
+        self.BP_LV_UP_spend = self.primo_to_usd(self.BP_LV_UP_spend_primo)
+
+        self.total_spend += self.BP_LV_UP_spend 
+
+    def Daly_resin_refill(self):
+        self.resin_refill_limit = 5
+        self.resin_refil_spend_primo = self.resin_refill_limit * self.days_from_releas
+        self.resin_refil_spend = self.primo_to_usd(self.resin_refil_spend_primo)
+
+        self.total_spend += self.resin_refil_spend
+
+    def get_limited_character_count(self):
+            # requests
+        url = "https://genshin-db-api.vercel.app/api/characters?query=5&matchCategories=true"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            characters = list(response.json())
+        else:
+            print("error, API don't work")
+            return []
+        
+            # Filter Standart 5*
+        limited5char = []
+        for char in characters:
+            if char in self.standart5char:
+                pass
+            else:
+                limited5char.append(char)
+        return limited5char
 
     def primo_to_usd(self, primo):
         bundles = self.prices["crystal"]
@@ -64,22 +94,17 @@ class whalecalculator:
 
     def main(self):
         print(f"""
-Welkin Moon
-ownd                {self.welkinowned}
-spend               {self.spendwelkin:.2f} usd
+{"="*40}
+            Total Spend
+Welkin Moon         {self.welkin_moon_spend:.2f} usd
+Battle Pass         {self.BP_spend:.2f} usd
+Battle Pass levl up {self.BP_LV_UP_spend:.2f} usd
+Refil Resin         {self.resin_refil_spend:.2f} usd
 
-Battle Pass
-ownd                {self.batlepassownd}
-spend on bp         {self.spendbp:.2f} usd
-spend on levls      {self.spendbplv:.2f} usd
-
-Characters
-1 copy              {self.pulls1copy} usd
-C6                  {self.pullsC6} usd
-spend               {self.spendallchar:.2f} usd
-
-total               {self.total:.2f} usd
+total               {self.total_spend:.2f} usd
+{"="*40}
 """)
+
 
 if __name__ == "__main__":    
     wc = whalecalculator()
